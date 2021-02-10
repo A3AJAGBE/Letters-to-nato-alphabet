2b9fe874eecbf8c46894e8c9284efc1ca6f2220f
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-10 18:51:32 +0000

    Add conversion

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-
 import pandas as pd
+# Convert the file to dict
 data = pd.read_csv("nato_alphabet.csv")
 data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(data_dict)
 
+# User prompt
+word = input("Enter a word: ").upper()
+
+# Convert word to nato phonetics
+response = [data_dict[letter] for letter in word]
+print(response)
 
