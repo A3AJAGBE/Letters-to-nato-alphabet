7cf8deb223627dd1a41e14078398cfe96ddd11d1
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-14 06:07:48 +0000

    Add try except to word convert function

--- main.py
+++ main.py
@@ -6,10 +6,22 @@ data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 print('Easily communicate a word over the phone.\n')
 
-# User prompt
-word = input("Enter a word: ").upper()
 
-# Convert word to nato phonetics
-response = [data_dict[letter] for letter in word]
-print(response)
+def word_convert():
+    """This function convert the word to nato alphabet"""
+    # User prompt
+    word = input("Enter a word: ").upper()
+
+    # prevent input that's not a letter
+    try:
+        # Convert word to nato phonetics
+        response = [data_dict[letter] for letter in word]
+        print(response)
+    except KeyError:
+        print("Invalid input, only letters in the alphabet.\n")
+        word_convert()
+
+
+word_convert()
+
 
